8da00d5b8b679942182d389a947d00233485e736
Yevhenii3145 <100586000+Yevhenii3145@users.noreply.github.com>
2022-07-07 02:54:32 +0300

    I have done )

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,25 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.body;
+
+const buttonRef = document.querySelector(".change-color");
+
+const spanRef = document.querySelector(".color");
+
+buttonRef.addEventListener("click", onButtonRefClick);
+
+function onButtonRefClick() {
+  const currentBodyColor = getRandomHexColor();
+  bodyRef.style.backgroundColor = currentBodyColor;
+  spanRef.textContent = currentBodyColor;
+
+  const currentBtnColor = getRandomHexColor();
+  buttonRef.style.backgroundColor = currentBtnColor;
+  if (buttonRef.textContent !== "Change color") {
+    buttonRef.textContent = "Change colors: " + currentBtnColor;
+    return;
+  }
+  buttonRef.textContent += `s: ${currentBtnColor}`;
+}
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,37 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputOfQuantityRef = document.querySelector("#controls > input");
+
+const createBtnRef = inputOfQuantityRef.nextElementSibling;
+
+const destroyBtnRef = createBtnRef.nextElementSibling;
+
+const targetBoxRef = document.querySelector("#boxes");
+
+createBtnRef.addEventListener("click", onCreateBtnRefClick);
+
+destroyBtnRef.addEventListener("click", destroyBoxes);
+
+function onCreateBtnRefClick() {
+  createBoxes.call(inputOfQuantityRef, inputOfQuantityRef.value);
+}
+
+function destroyBoxes() {
+  targetBoxRef.innerHTML = "";
+}
+
+function createBoxes(amount) {
+  const allSquares = [];
+  let sizeOfDiv = "30px";
+  for (let i = 1; i <= amount; i += 1) {
+    const div = document.createElement("div");
+    div.style.width = sizeOfDiv;
+    div.style.height = sizeOfDiv;
+    div.style.backgroundColor = getRandomHexColor();
+    allSquares.push(div);
+    sizeOfDiv = parseInt(sizeOfDiv) + 10 + "px";
+  }
+  targetBoxRef.append(...allSquares);
+}
